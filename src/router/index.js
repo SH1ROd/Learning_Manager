8917272f37185d0783260b0fd5d5b36298186b23
Dexter.L <863444090@qq.com>
2023-08-31 13:40:12 +0800

    2023年8月31日13:40:29-->demo01-->template

--- src/router/index.js
+++ src/router/index.js
@@ -49,9 +49,9 @@ export const constantRoutes = [
     redirect: '/dashboard',
     children: [{
       path: 'dashboard',
-      name: 'Dashboard',
+      name: 'Learning Manager',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: 'Learning Manager', icon: 'dashboard' }
     }]
   },
 
